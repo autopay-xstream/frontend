daa06047cab9c0c9d288cc3cd901010c423158d8
Aditya <aditya26sg@gmail.com>
2023-02-19 22:58:38 +0530

    logging notifications

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import {
   lightTheme,
 } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
+import { mainnet, polygon, optimism, arbitrum, goerli, polygonMumbai } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
@@ -27,11 +27,19 @@ import Notifications from "@/components/Notifications";
 
 //******************************************* */
 
+const Web3 = require("web3");
+const abiDecoder = require("abi-decoder");
+
+const web3 = new Web3("wss://polygon-mumbai.g.alchemy.com/v2/qac25z9Oep-F5rDu38yVfQHf5LjdJoGL");
+
+const destinationAbi = require("../data/destinationAbi.json");
+const destinationAddress = ["0xA73Bf7955fAae6Da0561F25bEA45F3d2D2119997"];
+
 export default function Home() {
   //********************** connect wallet imports
 
   const { chains, provider } = configureChains(
-    [polygon, goerli],
+    [polygonMumbai, goerli],
     [
       publicProvider(),
     ]
@@ -47,6 +55,66 @@ export default function Home() {
     connectors,
     provider,
   });
+
+  const [streamNotifications, setStreamNotifications] = useState([]);
+
+  useEffect(() => {
+    const run = async () => {
+      abiDecoder.addABI(destinationAbi);
+      const options = { address: destinationAddress }
+      var destinationEvents = web3.eth.subscribe('logs', options, function (error, result) {
+        if (!error)
+          console.log("Got result");
+        else
+          console.log(error);
+      }).on("data", async function (log) {
+        const decodedLogs = abiDecoder.decodeLogs([log]);
+        let eventName = decodedLogs[0]?.name;
+        let destinationAddress = decodedLogs[0]?.address;
+        let destinationLogs = decodedLogs[0].events
+        const eventNotification = { eventName: eventName, contractAddress: destinationAddress, events: destinationLogs }
+
+        console.log("Decoded Logs ", eventNotification);
+        let notificationPayload = {};
+        if (eventName == "StreamStart") {
+          // show a start stream event
+          notificationPayload = {
+            status: "initialise",
+            txnHash: "",
+            receipient: eventNotification?.events[1]?.value,
+            flowRate: eventNotification?.events[2]?.value,
+            amount: eventNotification?.events[3]?.value
+          }
+          console.log("Cookie data ", notificationPayload);
+        } else if (eventName == "StreamUpdate") {
+          notificationPayload = {
+            status: "Updated",
+            txnHash: "",
+            receipient: eventNotification?.events[1]?.value,
+            flowRate: eventNotification?.events[2]?.value,
+            amount: eventNotification?.events[3]?.value
+          }
+          // show a update stream event
+        } else {
+          notificationPayload = {
+            status: "Closed",
+            txnHash: "",
+            receipient: eventNotification?.events[1]?.value,
+            flowRate: eventNotification?.events[2]?.value,
+            amount: eventNotification?.events[3]?.value
+          }
+          // show a delete stream event
+        }
+        setStreamNotifications([notificationPayload, ...streamNotifications]);
+
+        // before: was using contractAddress as the channel id
+        // after: channel id is hardcoded now
+      }).on("changed", function (log) {
+        console.log("Changed");
+      });
+    }
+    run();
+  }, [])
 
   //********************** connect wallet imports
 
@@ -136,7 +204,7 @@ export default function Home() {
                   ) : showXStream ? (
                     <SendXStream />
                   ) : showNotification ?
-                    <Notifications />
+                    <Notifications notifications = {streamNotifications} />
                     : null
                   }
 
